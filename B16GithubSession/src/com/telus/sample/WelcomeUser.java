563bdb093ce2d846c54de0e151f03f518a318683
PCNAME <PCNAME@Telus>
2022-11-29 17:07:41 +0530

    sysout message added

--- B16GithubSession/src/com/telus/sample/WelcomeUser.java
+++ B16GithubSession/src/com/telus/sample/WelcomeUser.java
@@ -5,6 +5,9 @@ public class WelcomeUser {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Github session");
+		
+		// Code added by Pranav
+		System.out.println("Thank you for sharing");
 	}
 
 }
