0d610e3d7aa41d437532559ee3157fb314754668
Vinoth R <vinothrwins@gmail.com>
2022-11-29 16:54:32 +0530

    Uploading new project

---
+++ B16GithubSession/src/com/telus/sample/WelcomeUser.java
@@ -0,0 +1,10 @@
+package com.telus.sample;
+
+public class WelcomeUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Github session");
+	}
+
+}
